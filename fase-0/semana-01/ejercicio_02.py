cc3121788d993819964766e9e4e4d78db51b2018
ruben <arcticessence@gmail.com>
2026-09-12 01:09:31 +0100

    Add second Python exercise

---
+++ fase-0/semana-01/ejercicio_02.py
@@ -0,0 +1,9 @@
+producto = "teclado"
+precio = 25
+cantidad = 3
+
+total = precio * cantidad
+
+print(f"Has comprado {cantidad} unidades de {producto}.")
+print(f"Precio por unidad: {precio}.")
+print(f"Total: {total}€")
