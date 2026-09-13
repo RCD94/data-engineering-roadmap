e9f6def2ce46c84e0b784a97da6c7ca008731f12
ruben <arcticessence@gmail.com>
2026-09-13 19:40:11 +0100

    Exercises 3,4,5

---
+++ fase-0/semana-01/ejercicio_03.py
@@ -0,0 +1,13 @@
+producto = "Monitor"
+precio = 100
+cantidad = 2
+subtotal = precio * cantidad
+descuento = subtotal * 0.1
+total = subtotal - descuento
+
+print(f"Producto: {producto}")
+print(f"Precio: {precio}€")
+print(f"Cantidad: {cantidad}")
+print(f"Subtotal: {subtotal}€")
+print(f"Descuento: {descuento}€")
+print(f"Total: {total}€")
---
+++ fase-0/semana-01/ejercicio_05.py
@@ -0,0 +1,10 @@
+nota = 7
+
+if nota >= 9:
+    print("Sobresaliente")
+elif nota >= 7:
+    print("Notable")
+elif nota >= 5:
+    print("Aprobado")
+else:
+    print("Suspenso")
